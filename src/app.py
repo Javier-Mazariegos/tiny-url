cad47f7cb718e27648cfb84793aaf9caf76b6fe6
Javier Mazariegos <javiermazariegos@ufm.edu>
2020-11-20 19:10:26 -0600

    funcion de crear

--- src/app.py
+++ src/app.py
@@ -3,20 +3,45 @@ from jinja2 import Template, Environment, FileSystemLoader
 import redis
 import time
 import os
+from redisworks import Root
 
 file_loader = FileSystemLoader('templates')
 env = Environment(loader=file_loader)
-REDIS_HOST = os.getenv("REDIS_HOST", None)
-cache = redis.Redis(host=REDIS_HOST, port=6379)
+#REDIS_HOST = os.getenv("REDIS_HOST", None)
+#cache = redis.Redis(host=REDIS_HOST, port=6379)
+
+conn = redis.Redis('localhost', port=6379, charset="utf-8", decode_responses=True)
+l = Root()
+diccionarioURL = dict(l.something)
+
 
 app = Flask(__name__)
 
+
+def existe(custom): 
+    bandera = -1
+    for key,value in diccionarioURL.items():
+        if key == custom:
+            bandera = 1
+            break
+    return bandera
 #Tiny Urls
 @app.route('/')
 def tiny():
     template = env.get_template("index.html")
+    return template.render(diccionario = diccionarioURL)
 
-    return template.render()
+@app.route('/crear', methods=['POST'])
+def crear():
+    if request.method == 'POST':
+        url = request.form['url']
+        customid = request.form['customid']
+        if existe(customid) == -1:
+            diccionario2 = {customid:{"URL":url,"visitas": "0"}}
+            diccionarioURL.update(diccionario2)
+            return redirect(url_for('tiny', 201))
+        else:
+            return redirect(url_for('tiny', 400))
 
 #Url List
 @app.route('/urls')
