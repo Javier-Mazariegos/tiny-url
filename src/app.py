9519d9b568af5380d6b22816c70d6347cb60f96f
Javier Mazariegos <javiermazariegos@ufm.edu>
2020-11-23 19:25:49 -0600

    metodos de redis

--- src/app.py
+++ src/app.py
@@ -11,11 +11,31 @@ env = Environment(loader=file_loader)
 #REDIS_HOST = os.getenv("REDIS_HOST", None)
 #cache = redis.Redis(host=REDIS_HOST, port=6379)
 
-conn = redis.Redis('localhost', port=6379, charset="utf-8", decode_responses=True)
-l = Root()
-diccionarioURL = l.the.mapping.example
-if(len(diccionarioURL) == 0):
-    diccionarioURL = {}
+conn = redis.StrictRedis('localhost', port=6379, charset="utf-8", decode_responses=True)
+
+#con esta funcion se sobreescribe
+#hola1 = "URL"
+#hola2 = "www.google.com"
+#conn.hset("diccionario1","2", "{'URL': 'yahoo', 'visitas': '10'}") #---este es para escribir uno nuevo
+
+
+#dic = eval(conn.hget("diccionario1", "1")) ------
+#dic["visitas"] = "0"                       --------- Todo esto es para editar las visitas o un url
+#conn.hset("diccionario1","1", str(dic))    ------
+
+
+#conn.hdel("diccionario1","1[visitas]") ----- Este es para borrar un registro
+
+#conn.flushall() ----Este es para borrar todas las keys
+
+#print(conn.hgetall("diccionario1")) ----------- este es para obtener todo el diccionario
+
+#print(conn.hkeys("diccionario1"))  ------------ este es para obtener todas las llaves
+
+#print(conn.hvals("diccionario1"))   ------------ este es para obtener todos los valores
+
+#if(len(diccionarioURL) == 0):
+#    diccionarioURL = {}
 bandera = 0
 app = Flask(__name__)
 
@@ -36,6 +56,14 @@ def existe(custom):
                 bandera = 1
                 break
     return bandera
+
+def contar(customid):
+    vistas = 0
+    for k,v in diccionarioURL.items():
+        if customid == k:
+            vistas = v["visitas"]
+            vistas = int(vistas) + 1
+            v["visitas"] = vistas
 
 
 #Tiny Urls
@@ -61,15 +89,18 @@ def crear():
 @app.route('/urls')
 def urls():
     template = env.get_template('list.html')
-    print("==========================================")
-    print(l.the.mapping.example)
+    #print("==========================================")
+    #print(l.the.mapping.example)
     return template.render(diccionario = diccionarioURL)
 
 @app.route('/eliminar', methods=['POST'])
 def eliminar():
+    global l
     if request.method == 'POST':
         customid = request.form['customid']
-        l.the.mapping.example.__delitem__(customid)
+        l.delete(customid)
+        
+        #l.the.mapping.example.__delitem__(customid)
         return redirect(url_for('urls'), 301)
 
 
@@ -79,6 +110,11 @@ def eliminar():
 def stats():
     template = env.get_template('stats.html')
     return template.render(diccionario = diccionarioURL)
+
+@app.route('/<name>')   
+def ejemplo(name):
+    contar(name)
+    return redirect(url_for('stats'), 301)
 
 
 
